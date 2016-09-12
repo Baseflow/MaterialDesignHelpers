9b1aecbb15bc9d36580eaa065f0c9f83d053fcc8
Martijn van Dijk <mhvdijk@gmail.com>
2016-09-12 11:04:56 +1000

    Add initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# MaterialDesignHelpers
+Default colors and dimens per Material Design guidelines and Android Design guidelines inside one library.
+
+Xamarin version of material-design-dimens [library](https://github.com/DmitryMalkovich/material-design-dimens).
+
+Plugin is available on [Nuget](https://www.nuget.org/packages/Xam.Plugins.Android.MaterialDesignHelpers/).
+
+See the sample for implementation.
---
+++ Sample/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using Android.Support.V7.Widget;
+using Android.Views;
+using System;
+
+namespace Sample
+{
+    [Activity(Label = "MaterialDesignHelpers", MainLauncher = true, Icon = "@mipmap/icon", Theme="@style/AppTheme.NoActionBar")]
+    public class MainActivity : AppCompatActivity, View.IOnClickListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.SetOnClickListener(this);
+        }
+
+        public void OnClick(View view)
+        {
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                    .SetAction("Action", (obj) => { }).Show();
+        }
+
+        public override bool OnCreateOptionsMenu(Android.Views.IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(Android.Views.IMenuItem item)
+        {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.ItemId;
+
+            //noinspection SimplifiableIfStatement
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
+
